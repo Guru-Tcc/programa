37d5a5795aea1b43b91b47ee89d6b87c6f12a6c7
FELIPE DA SILVA GARCIA <garcia.f@alunoescolassociais.org.br>
2021-11-23 17:27:19 -0300

    inicializado phpmyadmin

--- banco_login/usuarios.php
+++ banco_login/usuarios.php
@@ -1,42 +1,39 @@
 <?php
-Class Usuario
+class Usuario
 {
-    private $pdo;
-    public $msgERRO ="";    
+     private $pdo;
+     public $msgERRO = "";
 
-    public function conectar($nome, $host, $usuario, $senha) 
-    {
-         global $pdo;
-         global $msgERRO;
+     public function conectar($nome, $host, $usuario, $senha)
+     {
+          global $pdo;
+          global $msgERRO;
 
-         try
-         {
-            $pdo = new PDO("mysql:dbname=".$nome.",host=".$host,$usuario,$senha);
-        }catch(PDOException $e){
-           $msgERRO = $e->getMessage();
-        }
-    }        
-        public function logar($nome, $senha)
-    {
-         global $pdo;
-         //verificar se o usuario e senha estao cadastrados,se sim
-         $sql = $pdo->prepare("SELECT id FROM usuarios WHERE nome = :n AND senha =:s");
-         $sql->bindValue(":n",$nome);
-         $sql->bindValue(":s",md5($senha));
-         $sql->execute();
-               //consulta ao BD
-         if($sql->rowCount() > 0)
-         {
-              //entrar no sistema (sessao)
-              $dado =$sql->fetch();
-              session_start();
-              $_SESSION['id'] =$dado['id'];
-              return true ; //logado com sucesso
-         }else{
-              return false;//não foi possivel logar
-              header("location:../index.php");
-         }
-         
-    }
-   
+          try {
+               $pdo = new PDO("mysql:dbname=" . $nome . ",host=" . $host, $usuario, $senha);
+          } catch (PDOException $e) {
+               $msgERRO = $e->getMessage();
+          }
+     }
+     public function logar($nome, $senha)
+     {
+          global $pdo;
+          global $msgERRO;
+          //verificar se o usuario e senha estao cadastrados,se sim
+          $sql = $pdo->prepare("SELECT id FROM usuarios WHERE nome = :n AND senha =:s");
+          $sql->bindValue(":n", $nome);
+          $sql->bindValue(":s", md5($senha));
+          $sql->execute();
+          //consulta ao BD
+          if ($sql->rowCount() > 0) {
+               //entrar no sistema (sessao)
+               $dado = $sql->fetch();
+               session_start();
+               $_SESSION['id'] = $dado['id'];
+               return true; //logado com sucesso
+          } else {
+               return false; //não foi possivel logar
+               header("location:../index.php");
+          }
+     }
 }
